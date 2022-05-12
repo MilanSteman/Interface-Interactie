e1f72a6c74db508d3e1ca3def45a63aff76b173c
milansteman <milansteman@gmail.com>
2022-05-12 11:27:59 +0200

    Added 3d slide-rectangle to middle part

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,5 @@
-//
+const rightOpenRectangle = document.querySelector("article>section:nth-of-type(4)");
+
+rightOpenRectangle.addEventListener("click", () => {
+    rightOpenRectangle.classList.toggle("open");
+});
