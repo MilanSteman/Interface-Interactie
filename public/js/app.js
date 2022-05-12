ec70644fd17a225efa2ba11debaeb3b447c8f135
milansteman <milansteman@gmail.com>
2022-05-12 17:35:14 +0200

    Fully 3d working model

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 const rightOpenRectangle = document.querySelector("article>section:nth-of-type(4)");
+const topOpenRectangle = document.querySelector("article>section:nth-of-type(5)");
+
+topOpenRectangle.addEventListener("click", () => {
+    topOpenRectangle.classList.toggle("open");
+});
 
 rightOpenRectangle.addEventListener("click", () => {
     rightOpenRectangle.classList.toggle("open");
