3413004afba627df122086c27be290bfa3cde699
milansteman <milansteman@gmail.com>
2022-05-16 14:55:26 +0200

    Added scrolling to the screen list

--- public/js/app.js
+++ public/js/app.js
@@ -8,11 +8,24 @@ const dPadDown = document.querySelector("article > section:nth-of-type(2) > ol >
 
 const dPad = document.querySelector("article > section:nth-of-type(2) > ol > li:nth-of-type(1) section:nth-of-type(1) ol");
 
+const pokeListBottom = document.querySelector("article > section:nth-of-type(1) > ol:nth-of-type(1) > li:nth-of-type(1) > section ol");
+const pokeListBottomAllChildren = document.querySelectorAll("article > section:nth-of-type(1) > ol:nth-of-type(1) > li:nth-of-type(1) > section ol > li");
+let pokeListBottomCounter = 0;
+
 dPadLeft.addEventListener("mousedown", () => dPad.classList.add("left"));
 dPadLeft.addEventListener("mouseup", () => dPad.classList.remove("left"));
 dPadLeft.addEventListener("mouseout", () => dPad.classList.remove("left"));
 
-dPadUp.addEventListener("mousedown", () => dPad.classList.add("up"));
+dPadUp.addEventListener("mousedown", () => {
+    dPad.classList.add("up");
+    if (pokeListBottomCounter === 0) {
+        pokeListBottomCounter = pokeListBottomAllChildren.length - 1;
+    } else {
+        pokeListBottomCounter--;
+    } 
+    pokeListBottom.scroll({top: pokeListBottomAllChildren[pokeListBottomCounter].offsetTop, left: 0, behavior: 'smooth'});
+    console.log(pokeListBottomCounter);
+});
 dPadUp.addEventListener("mouseup", () => dPad.classList.remove("up"));
 dPadUp.addEventListener("mouseout", () => dPad.classList.remove("up"));
 
@@ -20,7 +33,18 @@ dPadRight.addEventListener("mousedown", () => dPad.classList.add("right"));
 dPadRight.addEventListener("mouseup", () => dPad.classList.remove("right"));
 dPadRight.addEventListener("mouseout", () => dPad.classList.remove("right"));
 
-dPadDown.addEventListener("mousedown", () => dPad.classList.add("down"));
+dPadDown.addEventListener("mousedown", () => {
+    dPad.classList.add("down");
+    console.log(pokeListBottomAllChildren.length)
+    if (pokeListBottomCounter === pokeListBottomAllChildren.length - 1) {
+        pokeListBottomCounter = 0;
+    } else {
+        pokeListBottomCounter++;
+    }
+    pokeListBottom.scroll({top: pokeListBottomAllChildren[pokeListBottomCounter].offsetTop, left: 0, behavior: 'smooth'});
+    console.log(pokeListBottomCounter);
+
+});
 dPadDown.addEventListener("mouseup", () => dPad.classList.remove("down"));
 dPadDown.addEventListener("mouseout", () => dPad.classList.remove("down"));
 
