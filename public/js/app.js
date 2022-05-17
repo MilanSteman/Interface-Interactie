fa703767379718f706758879c0a7c59d0570ebf6
milansteman <milansteman@gmail.com>
2022-05-17 16:27:37 +0200

    Extra functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+const background = document.querySelector("body");
 const rightOpenRectangle = document.querySelector("article>section:nth-of-type(4)");
 const topOpenRectangle = document.querySelector("article>section:nth-of-type(5)");
 
@@ -18,11 +19,19 @@ const pokeListBottom = document.querySelector("article > section:nth-of-type(1) 
 const pokeListBottomChildren = document.querySelectorAll("article > section:nth-of-type(1) > ol:nth-of-type(1) > li:nth-of-type(1) > section ol > li");
 
 const longButton = document.querySelector("article > section:nth-of-type(2) > ol > li:nth-of-type(1) section:nth-of-type(1) > ol:nth-of-type(1) li:nth-of-type(1) button");
+const typeArray = ["normal", "water", "electric", "fire", "psychic", "dark", "grass", "ice", "fairy"];
+
 let pokeCounter = 0;
 
 longButton.addEventListener("click", () => {
     screenTop.classList.toggle("online");
     screenBottom.classList.toggle("online");
+
+    if (background.hasAttribute("class")) {
+        background.removeAttribute("class");
+    } else {
+        background.classList.add(typeArray[pokeCounter]);
+    }
 });
 
 dPadLeft.addEventListener("mousedown", () => dPad.classList.add("left"));
@@ -31,52 +40,67 @@ dPadLeft.addEventListener("mouseout", () => dPad.classList.remove("left"));
 
 const goNext = () => {
     dPad.classList.add("down");
-    if (pokeCounter === pokeListBottomChildren.length - 1) {
-        pokeCounter = 0;
-    } else {
-        pokeCounter++;
-    }
+    if (background.hasAttribute("class")) {
 
-    for (let i = 0; i < pokeListTopChildren.length; i++) {
-        if (i === pokeCounter) {
-            pokeListTopChildren[i].classList.add("current");
+        if (pokeCounter === pokeListBottomChildren.length - 1) {
+            pokeCounter = 0;
         } else {
-            pokeListTopChildren[i].classList.remove("current");
+            pokeCounter++;
         }
-    }
 
-    pokeListTop.scroll({ top: pokeListTopChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
-    pokeListBottom.scroll({ top: pokeListBottomChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
+        for (let i = 0; i < pokeListTopChildren.length; i++) {
+            if (i === pokeCounter) {
+                pokeListTopChildren[i].classList.add("current");
+            } else {
+                pokeListTopChildren[i].classList.remove("current");
+            }
+        }
+
+        background.removeAttribute("class");
+        background.classList.add(typeArray[pokeCounter]);
+
+        pokeListTop.scroll({ top: pokeListTopChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
+        pokeListBottom.scroll({ top: pokeListBottomChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
+    }
 }
 
 const goPrevious = () => {
     dPad.classList.add("up");
-    if (pokeCounter === 0) {
-        pokeCounter = pokeListBottomChildren.length - 1;
+    if (background.hasAttribute("class")) {
 
-        for (let i = 0; i < pokeListTopChildren.length; i++) {
-            if (i === pokeCounter) {
-                pokeListTopChildren[i].classList.add("current");
-            } else {
-                pokeListTopChildren[i].classList.remove("current");
+        if (pokeCounter === 0) {
+            pokeCounter = pokeListBottomChildren.length - 1;
+
+            for (let i = 0; i < pokeListTopChildren.length; i++) {
+                if (i === pokeCounter) {
+                    pokeListTopChildren[i].classList.add("current");
+                } else {
+                    pokeListTopChildren[i].classList.remove("current");
+                }
             }
-        }
 
-        pokeListTop.scroll({ top: pokeListTopChildren[pokeCounter].offsetTop, left: 0, behavior: 'smooth' });
-        pokeListBottom.scroll({ top: pokeListBottomChildren[pokeCounter].offsetTop, left: 0, behavior: 'smooth' });
-    } else {
-        pokeCounter--;
+            background.removeAttribute("class");
+            background.classList.add(typeArray[pokeCounter]);
 
-        for (let i = 0; i < pokeListTopChildren.length; i++) {
-            if (i === pokeCounter) {
-                pokeListTopChildren[i].classList.add("current");
-            } else {
-                pokeListTopChildren[i].classList.remove("current");
+            pokeListTop.scroll({ top: pokeListTopChildren[pokeCounter].offsetTop, left: 0, behavior: 'smooth' });
+            pokeListBottom.scroll({ top: pokeListBottomChildren[pokeCounter].offsetTop, left: 0, behavior: 'smooth' });
+        } else {
+            pokeCounter--;
+
+            for (let i = 0; i < pokeListTopChildren.length; i++) {
+                if (i === pokeCounter) {
+                    pokeListTopChildren[i].classList.add("current");
+                } else {
+                    pokeListTopChildren[i].classList.remove("current");
+                }
             }
-        }
 
-        pokeListTop.scroll({ top: pokeListTopChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
-        pokeListBottom.scroll({ top: pokeListBottomChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
+            background.removeAttribute("class");
+            background.classList.add(typeArray[pokeCounter]);
+
+            pokeListTop.scroll({ top: pokeListTopChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
+            pokeListBottom.scroll({ top: pokeListBottomChildren[pokeCounter].offsetTop - 10, left: 0, behavior: 'smooth' });
+        }
     }
 }
 
@@ -158,11 +182,9 @@ document.addEventListener("keyup", (event) => {
                 topOpenRectangle.classList.toggle("open");
                 break;
             case dPadUp:
-                console.log("test")
                 goPrevious();
                 break;
             case dPadDown:
-                console.log("test")
                 goNext();
                 break;
         }
